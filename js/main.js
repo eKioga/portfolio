f58fcca56fde965b53acac7de7b2f3db289503ac
Eric Post <erickpost@gmail.com>
2017-10-17 18:53:18 -0700

    updated the large index text to more reflext helpdesk related stuff. I also added the helpdesk automation project to the portfolio, will expand on it's description later

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ import ExampleWork from './example-work';
 
 const myWork = [
     {
+        'title': "IT Helpdesk: Automation and streamlining" ,
+        'href': "",
+        'desc': "During my last position, I Reduced technical debt along with break/fix tickets by automating all critical Help Desk functions. This includes: user creation, asset management and PC imaging along with all software reporting, deployment and patching. Built alert and reporting systems for the monitoring of both network and node health. Problems would trigger alerts, which in turn, trigger solutions. ",
+        'image': {
+            'desc': "Screenshot something i have yet to add",
+            'src': "images/helpdesk_auto.jpg",
+            'comment': ""
+        }
+    },
+    {
         'title': "My Ruby on Rails Blog/Portfolio app",
         'href': "https://github.com/eKioga/blog",
         'desc': "I consider Ruby as my first real foray into programming. I picked up Rails soon after as a way of practicing both DevOps tech like Ansible and code deployment tools like Capistrano. Lots of fun! This was my main blog/portfolio site until I learned about serverless tech with AWS. I became so engrossed with that tech that I've since replaced it with the serverless portfolio site you're looking at now. It is currently being self-hosted on http://eric-post.ddns.net/ but it may have been decommissioned by the time you read this.",
